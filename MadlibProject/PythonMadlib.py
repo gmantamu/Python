70bf41c07a28ade6cdb0d2f39215be67687c1e65
Garrett Mandrona <gwmandrona@gmail.com>
2022-01-27 17:58:52 -0600

    first commit

---
+++ MadlibProject/PythonMadlib.py
@@ -0,0 +1,7 @@
+# Testing different ways to concatenate strings in Python
+# How can we add on to an existing string? How can we print it?
+
+# Different examples:
+# print("Happy " + birthday!)
+# print("Happy {}".format(birthday!))
+# print(f"Happy {birthday!}")
