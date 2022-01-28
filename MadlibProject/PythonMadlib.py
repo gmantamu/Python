7ab965ec832923f5eacf6449cd613a83d6af677c
gmantamu <gwmandrona@gmail.com>
2022-01-27 18:20:09 -0600

    added madlib paragraph

--- MadlibProject/PythonMadlib.py
+++ MadlibProject/PythonMadlib.py
@@ -10,3 +10,6 @@ Adj = input("Input an adjective: ")
 Verb1 = input("Input a verb: ")
 Verb2 = input("Input a second verb: ")
 Person = input("Input a person: ")
+
+finalMadlib = f"I truly learn to program because it is {Adj}. It is always a joy to learn new code, and whenever I code, my favorite past time is to {Verb1}. \
+    Second only to this, I also enjoy to {Verb2} while I code. My goal is to become like {Person}, so I can be very succesfull"
