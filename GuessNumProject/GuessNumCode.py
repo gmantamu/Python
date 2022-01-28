926f1faf18343dc44c7abfb661b91da26d89df18
gmantamu <gwmandrona@gmail.com>
2022-01-27 19:14:35 -0600

    completed basic random number game

--- GuessNumProject/GuessNumCode.py
+++ GuessNumProject/GuessNumCode.py
@@ -7,5 +7,10 @@ def guess(n):
     while guess != randNum:
         guess = int(input(f'Guess a number between 1 and {n}: '))
         print(guess)
+        if guess < randNum:
+            print("Guess is too low")
+        elif guess > randNum:
+            print("Guess is too high")
+    print(f"Correct! The random number is: {randNum}")
 
 guess(10)
