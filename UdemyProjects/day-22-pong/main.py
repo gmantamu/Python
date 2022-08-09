29fadefaf5682c72500601be1a286ec5a30af04b
Garrett Mandrona <85818166+gurt989@users.noreply.github.com>
2022-08-09 16:51:34 -0500

    adding the ball bouncing for when it touches either of the paddles

--- UdemyProjects/day-22-pong/main.py
+++ UdemyProjects/day-22-pong/main.py
@@ -30,5 +30,9 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce()   
 
+    #detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        print("Made contact")
+
 
 screen.exitonclick()
