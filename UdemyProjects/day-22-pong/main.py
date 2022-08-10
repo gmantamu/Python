3e5c2d8c1b149562c819879001bec1d50a0c5e94
Garrett Mandrona <85818166+gurt989@users.noreply.github.com>
2022-08-09 23:15:47 -0500

    added a new scoreboard class, which counts the points for each player

--- UdemyProjects/day-22-pong/main.py
+++ UdemyProjects/day-22-pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -24,7 +26,7 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    ball.move()
+    ball.move() 
 
     #Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
@@ -37,9 +39,11 @@ while game_is_on:
     #Detect when right paddle missed
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     #Detect when left paddle missed
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
