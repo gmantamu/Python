ede954640b8b74e8a4dd63b07250773eb75d2278
Garrett Mandrona <85818166+gurt989@users.noreply.github.com>
2022-08-14 10:57:17 -0500

    addes the OS module to the scripting practice, added code to both modules so far

---
+++ PythonScriptingModules/OSModule.py
@@ -0,0 +1,8 @@
+import os
+
+#prints current working directory
+print(os.getcwd())
+
+#changes the directory we are operating in
+os.chdir("add some type of file path in here (suhc ad D:/user/pythonthing/pythonfile")
+
---
+++ PythonScriptingModules/SysModule.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.version)
+
+#can use commands such as std::out and std::in to receive information from the users interacting with the program
