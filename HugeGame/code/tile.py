fcb253b47542b39028ea826dc4eeb81a64b31803
Garrett Mandrona <85818166+gurt989@users.noreply.github.com>
2022-09-04 16:06:03 -0500

    made changes to the tile file

--- HugeGame/code/tile.py
+++ HugeGame/code/tile.py
@@ -1,5 +1,8 @@
 import pygame
 from settings import *
 
-class Tile(pygame.sprite.sprite):
-    def __init__(self,pos,groups)
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
